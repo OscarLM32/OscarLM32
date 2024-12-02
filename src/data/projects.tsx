42d796e81dd6aadf8858781b67545fff95afbc91
Óscar López <oscarlopezmaldonado32@gmail.com>
2024-12-02 12:40:35 +0100

    Added projects images

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,3 +1,6 @@
+import runTWebAppImage from "../assets/RunT-web-app-login.png"
+import runTApiImage from "../assets/RunT-API-docs.png"
+
 export interface Project {
   id: number;
   title: string;
@@ -14,7 +17,7 @@ export const projects: Project[] = [
     id: 1,
     title: "RunT Web App",
     description: "A web app that helps hobbyist and professional runners find groups and events in their local area.",
-    image: "https://via.placeholder.com/600x400",
+    image: runTWebAppImage,
     technologies: ["React", "Axios", "BootStrap", "React-Query", "JavaScript"],
     github: "https://github.com/OscarLM32/RunningGroupWebApp",
     features: [
@@ -29,7 +32,7 @@ export const projects: Project[] = [
     id: 2,
     title: "RunT API",
     description: "API that handles all the server-side logic of an app that brings people that enjoy running together.",
-    image: "https://via.placeholder.com/600x400",
+    image: runTApiImage,
     technologies: ["ASP.NET", "RESTful", "AI", "MySQL", "Entity Framework"],
     github: "https://github.com/OscarLM32/RunningGroupAPI",
     features: [
